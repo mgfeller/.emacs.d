59f5efbbbae53357570d7b54dc54556c1a22ada8
Michael Gfeller <mgfeller@mgfeller.net>
2018-11-09 14:00:59 +0100

    added ido and web-mode

--- init.el
+++ init.el
@@ -1,1 +1,19 @@
-(set-face-foreground 'minibuffer-prompt "white") 
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+(set-face-foreground 'minibuffer-prompt "white")
+
+;; web-mode
+;; wget https://raw.githubusercontent.com/fxbois/web-mode/master/web-mode.el
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.jinja\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+
+
+;; Ido mode
+;; https://wikemacs.org/wiki/Ido
+
+(ido-mode)
+(ido-everywhere 1)
+(setq ido-case-fold t)
+
+
