04ce1357cd0b14cea080053f2d919292a3a66f79
Michael Gfeller <mgfeller@mgfeller.net>
2019-01-20 18:47:04 +0100

    added tools for python development

--- init.el
+++ init.el
@@ -1,3 +1,47 @@
+;; some from https://realpython.com/emacs-the-best-python-editor/
+(require 'package)
+
+(add-to-list 'package-archives
+       '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar myPackages
+  '(better-defaults
+    flycheck
+    elpy ;; python https://elpy.readthedocs.io/en/latest/ide.html
+    py-autopep8 ;; requires autopep8 installed using e.g. pip
+    material-theme))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+(setq inhibit-startup-message t) ;; hide the startup message
+(load-theme 'material t) ;; load material theme
+
+(setq linum-format "%4d ")
+(global-linum-mode t) ;; enable line numbers globally
+
+;; enable elpy python and related packages
+(elpy-enable)
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; https://elpy.readthedocs.io/en/latest/ide.html#interpreter-setup
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i --simple-prompt")
+
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+;; 
+
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 (set-face-foreground 'minibuffer-prompt "white")
@@ -21,5 +65,3 @@
 (ido-mode)
 (ido-everywhere 1)
 (setq ido-case-fold t)
-
-
