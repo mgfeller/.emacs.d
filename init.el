7dd82e53885dbe9742866ca3d3d76d29ad3e2cff
Michael Gfeller <mgfeller@mgfeller.net>
2019-01-22 08:22:13 +0100

    added json mode package

--- init.el
+++ init.el
@@ -14,6 +14,7 @@
     flycheck
     elpy ;; python https://elpy.readthedocs.io/en/latest/ide.html
     py-autopep8 ;; requires autopep8 installed using e.g. pip
+    json-mode
     material-theme))
 
 (mapc #'(lambda (package)
@@ -65,3 +66,17 @@
 (ido-mode)
 (ido-everywhere 1)
 (setq ido-case-fold t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (material-theme py-autopep8 elpy flycheck better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
