71b636b21030fefa90505dfb3a6cb65f7dd118e6
Michael Gfeller <mgfeller@mgfeller.net>
2019-04-19 13:58:56 +0200

    installed magit, requires emacs 25.1

--- init.el
+++ init.el
@@ -40,6 +40,13 @@
 
 ;; (setq tab-stop-list (number-sequence 4 120 4))
 
+;; org mode
+;; The following lines are always needed.  Choose your own keys.
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-switchb)
+
 ;; scala-mode
 (require 'use-package)
 
