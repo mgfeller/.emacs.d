62dafb3a81832b4e033cbae46b56c22c0aa772c0
Michael Gfeller <mgfeller@mgfeller.net>
2019-08-09 12:29:11 +0200

    enabled recent files

--- init.el
+++ init.el
@@ -38,6 +38,14 @@
             (setq tab-width 2)))
 
 
+;; file stuff
+;; recent files
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(run-at-time nil (* 5 60) 'recentf-save-list)
+
 ;; tab stuff
 (setq default-tab-width 2)
 
